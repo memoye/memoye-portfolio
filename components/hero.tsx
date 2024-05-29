5659846c020f7e2a9fdabab1206c1798a97fa630
Brown <memoyebrown@gmail.com>
2024-05-29 02:01:34 +0100

    setting up

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,22 @@
+import { Spotlight } from "./ui/spotlight";
+
 export default function Hero() {
   return (
-    <div className="pb-20 pt-36">
-      <div></div>
+    <div className="bg-background pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-left-40 -top-40 h-screen md:-left-32 md:-top-0"
+          fill="#FFFFFF"
+        />
+        <Spotlight
+          className="-left-full -top-10 h-[80vh] w-[50vw] md:-left-32 md:-top-20"
+          fill="#00A36C"
+        />
+        <Spotlight
+          className="-left-80 top-28 h-[80vh] w-[50vw]"
+          fill="#00FFD1"
+        />
+      </div>
     </div>
   );
 }
